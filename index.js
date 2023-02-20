328669c0289a8c74455976390c5c292e0fb683b7
Justin-Brueske <119259409+Justin-Brueske@users.noreply.github.com>
2023-02-19 20:47:55 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const team = [];
+
+const addManager = [
+    // questions
+];
+const addEngineer = [
+    // questions
+];
+const addIntern = [
+    // questions
+];
 
